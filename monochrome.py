92168edef9fa554f92de3966a42b017713677728
Ryan Niemi <40528017+J3T4R0@users.noreply.github.com>
2020-04-14 18:24:50 -0700

    Made it truly monochrome

--- monochrome.py
+++ monochrome.py
@@ -1,31 +1,15 @@
 from PIL import Image
-
-def make_sepia_palette(color):
-    palette = []
-    r, g, b = color
-    for i in range(255):
-        palette.extend((r*i/255, g*i/255, b*i/255))
-        
-    return palette
-
-def create_sepia(input_image_path,
-    output_image_path):
-    whitish = (255, 240, 192)
-    sepia = make_sepia_palette(whitish)
-    
+def to_monochrome(input_image_path,
+    output_image_path,
+    dithering=True):
     color_image = Image.open(input_image_path)
-    
-    # convert our image to gray scale
-    bw = color_image.convert('L')
-    
-    # add the sepia toning
-    bw.putpalette(sepia)
-    
-    # convert to RGB for easier saving
-    sepia_image = bw.convert('RGB')
-    
-    sepia_image.save(output_image_path)
+    if dithering:
+        bw = color_image.convert('1')  
+    else:
+        bw = color_image.convert('1',
+    dither=Image.NONE)
+    bw.save(output_image_path)
 
 if __name__ == '__main__':
-    create_sepia('querkle.png',
+    to_monochrome('querkle.png',
                  'sepia_querkle.png')
